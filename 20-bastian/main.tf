90eecd6f2d4b6277097f78d5b2a9eff6c39df8fe
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-26 17:36:48 +0530

    terraform ec2 bastion

---
+++ 20-bastian/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "main" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.bastion_sg_id]
+    subnet_id = local.public_subnet_id
+
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.common_name}-bastian"
+        }
+    )
+
+
+    }
+
+  
