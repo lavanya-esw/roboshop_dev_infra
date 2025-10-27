9e0b7250040f6013969945d9c493cc615fc1e0df
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-27 23:52:32 +0530

    terraform adding sg rules

---
+++ 30-sg-rules/main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group_rule" "allow_frontend-alb_to_frontend" {
+    type = "ingress"
+    security_group_id = local.security_group_id
+    source_security_group_id = local.source_security_group_id
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  
+}
+
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  security_group_id = local.bastion_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  security_group_id = local.backend_security_group_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
---
+++ 30-sg-rules/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "6.16.0"
+
+    }
+    
+  }
+
+  backend "s3" {
+    bucket = "roboshop-dev-remote-state"
+    key = "valuremote-state-roboshop-sg-rules"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+    
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
