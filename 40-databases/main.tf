d0df6fade43b170de9dad5251f4c566ea7df56d6
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-30 11:13:20 +0530

    terraform mongodb connection from bastion

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -40,9 +40,9 @@ resource "terraform_data" "mongodb" {
 
     provisioner "remote-exec" {
     inline = [
-        # "chmod +x /tmp/bootstrap.sh",
-        # # "sudo sh /tmp/bootstrap.sh"
-        # "sudo sh /tmp/bootstrap.sh mongodb"
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mongodb",
 
         "echo 'mongodb conneected'"
     ]
