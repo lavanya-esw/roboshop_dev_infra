4832697fdafd31b949a3f3edd59cea1c596e95e1
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-30 11:32:07 +0530

    terraform mongodb connection from bastion

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 sudo dnf install ansible -y
-ansible-pull -U https://github.com/lavanya-esw/roboshop_ansible_roles_tf.git -e component=$component main.yaml
+#ansible-pull -U https://github.com/lavanya-esw/roboshop_ansible_roles_tf.git -e component=$component main.yaml
+REPO_URL="https://github.com/lavanya-esw/roboshop_ansible_roles_tf.git"
+REPO_DIR="/opt/roboshop/ansible"
+ANSIBLE_DIR="roboshop_ansible_roles_tf"
+
+LOG_DIR="/var/log/roboshop"
+mkdir -p ${LOG_DIR}
+cd $LOG_DIR
+ansible.log
+
+cd $REPO_DIR
+if [ -d $ANSIBLE_DIR ]; then
+    cd $ANSIBLE_DIR
+    git pull
+else
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+
+ansible-playbook -e "component=$component" -e "env=$environment" main.yaml
