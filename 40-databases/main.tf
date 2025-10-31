a278075f793f0a4a0fe227e422d022bac3125432
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-31 11:23:02 +0530

    terraform database creation

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "redis" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [ local.redis_security_group_id ]
-    subnet_id = local.private_subnet_id
+    subnet_id = local.database_subnet_id
 
     tags = merge(
         local.common_tags,
@@ -86,7 +86,7 @@ resource "aws_instance" "rabbitmq" {
     ami = local.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = [ local.rabbitmq_security_group_id ]
-    subnet_id = local.private_subnet_id
+    subnet_id = local.database_subnet_id
 
     tags = merge(
         local.common_tags,
@@ -123,3 +123,56 @@ resource "terraform_data" "rabbitmq" {
     
   }
 }
+
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ local.mysql_security_group_id ]
+    subnet_id = local.database_subnet_id
+    iam_instance_profile = aws_iam_instance_profile.mysql.name
+
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.common_name}-mysql"
+        }
+    )
+  
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+    name = "mysql"
+    role = "EC2SSMParameterRead"
+  
+}
+
+# resource "terraform_data" "mysql" {
+#     triggers_replace = [
+#         aws_instance.mysql.id
+#     ]
+
+#     connection {
+#       type = "ssh"
+#       user = "ec2-user"
+#       password = "DevOps321"
+#       host = aws_instance.mysql.private_ip
+#     }
+
+#     provisioner "file" {
+#         source = "./bootstrap.sh"
+#         destination = "/tmp/bootstrap.sh"
+      
+#     }
+
+#     provisioner "remote-exec" {
+#     inline = [
+#         "chmod +x /tmp/bootstrap.sh",
+#         "sudo sh /tmp/bootstrap.sh mysql dev",
+#         "echo 'mysql connected'"
+#       ]
+    
+#   }
+# }
+  
+
+  
