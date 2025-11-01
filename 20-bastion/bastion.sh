43f91630c3c8a0b8f7c5499410b18d6cc42882df
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-01 12:05:01 +0530

    terraform bastion creation

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -2,6 +2,10 @@
 exec > /var/log/user-data.log 2>&1
 set -x
 
+# growing the /home volume for terraform purpose
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/mapper/RootVG-homeVol 
+xfs_growfs /home
 echo "Starting user data script..."
 
 yum install -y yum-utils
@@ -9,3 +13,5 @@ yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.
 yum -y install terraform
 
 echo "Terraform installed successfully."
+
+
--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "main" {
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
     user_data = file("${path.module}/bastion.sh")
+    # need more for terraform
+    root_block_device {
+        volume_size = 50
+        volume_type = "gp3" # or "gp2", depending on your preference
+    }
 
     tags = merge(
         local.common_tags,
