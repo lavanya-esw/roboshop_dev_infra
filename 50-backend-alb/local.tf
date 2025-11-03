a8f67ebaeabea3f24e53a895e8bea6668603634f
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-03 13:19:18 +0530

    terraform backend  creation

--- 50-backend-alb/local.tf
+++ 50-backend-alb/local.tf
@@ -2,7 +2,7 @@ locals {
   security_group = data.aws_ssm_parameter.backend_alb_sg-id.value
   subnets = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   common_name = "${var.project}-${var.environment}"
-  zone_id = data.aws_route53_zone.zone_id.value
+  zone_id = data.aws_route53_zone.zone_id.id
 
   common_tags= {
     Project = var.project
