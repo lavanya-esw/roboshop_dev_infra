486d2de521d2d3cacbc10cc49d1fbfc76fcaa3b6
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-12 19:54:43 +0530

    terraform frontend alb creation

---
+++ 80-frontend-alb/data.tf
@@ -0,0 +1,17 @@
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+    name = "/${var.project}/${var.environment}/frontend_alb_sg_id" 
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project}/${var.environment}/public_subnet_ids"
+  
+}
+
+data "aws_ssm_parameter" "frontend_alb_certificate_arn" {
+    name = "/${var.project}/${var.environment}/frontend_alb_certificate_arn"
+  
+}
+
+data "aws_route53_zone" "zone_id" {
+  name = "awsdevops.fun" # Note the trailing dot for the FQDN
+}
