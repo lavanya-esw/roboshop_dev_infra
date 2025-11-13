2f10b4c35f7b975eb0f32fdd282a83b7c1e4c31c
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-13 22:12:25 +0530

    terraform roboshop components

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components"{
     for_each = var.components
     source = "git::https://github.com/lavanya-esw/terraform_roboshop_component.git?ref=main"
-    component = var.key
-    rule_priority = var.value.rule_priority
+    component = each.key
+    rule_priority = each.value.rule_priority
 }
