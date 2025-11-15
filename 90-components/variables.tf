fbde9ef9eec99b8df0fa1f01e0aa432f69c32703
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-15 10:33:46 +0530

    terraform roboshop components

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -10,14 +10,14 @@ variable "rule_priority" {
 
 variable "components" {
     default = {
-/*         catalogue = {
+         catalogue = {
             rule_priority = 10
         }
 
         user = {
             rule_priority = 20
-        } */
-
+        }
+ 
         cart = {
             rule_priority = 30
         }
