6ea2236bd9f0da0204fd7c915694f13041ff1456
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-15 20:12:20 +0530

    terraform roboshop components

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -54,4 +54,7 @@ data "aws_ssm_parameter" "frontend-alb_sg-id"{
     name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
 
+data "aws_ssm_parameter" "frontend_sg-id"{
+    name = "/${var.project}/${var.environment}/frontend_sg_id"
+}
 
--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -264,4 +264,32 @@ resource "aws_security_group_rule" "backend_alb_payment" {
   to_port           = 80
 }
 
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  security_group_id = local.frontend_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+resource "aws_security_group_rule" "frontend_frontend_alb" {
+  type              = "ingress"
+  security_group_id = local.frontend_sg_id
+  source_security_group_id = local.frontend_alb_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
+
+##### Frontend ALB SG RUles #####
+resource "aws_security_group_rule" "frontend_alb_public" {
+  type              = "ingress"
+  security_group_id = local.frontend_alb_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 443
+  protocol          = "tcp"
+  to_port           = 443
+}
+
 
