ad34f2e0092dfa1e99da4a6a33a2cd3b4e4fa67a
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-16 12:50:01 +0530

    sg rules

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -19,8 +19,8 @@ resource "aws_lb_listener" "frontend_alb" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06" # Or a more recent, secure policy
-  certificate_arn   = local.frontend_alb_certificate_arn# Replace with your ACM certificate ARN
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
+  certificate_arn   = local.frontend_alb_certificate_arn
 
   default_action {
     type = "fixed-response"
@@ -31,7 +31,6 @@ resource "aws_lb_listener" "frontend_alb" {
       status_code  = "200"
     }
   }
-
 }
 
 resource "aws_route53_record" "frontend_alb" {
